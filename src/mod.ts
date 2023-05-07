51954a65241541bbb10c501b385dfc15647afed4
lis2a <me@lilis.dev>
2023-05-08 01:38:18 +0900

    feat: getting_started endpoint

--- src/mod.ts
+++ src/mod.ts
@@ -8,10 +8,13 @@ const github = "https://github.com/codrawers";
 const twitter = "https://twitter.com/codrawers";
 const youtube = "https://youtube.com/@codrawers";
 const qiita = "https://qiita.com/organizations/codrawers";
+const gettingStarted =
+  "https://codrawers.notion.site/80a3ccb317b2420693ca10ada089c0ca";
 
 app.get("/github", (c) => c.redirect(github));
 app.get("/twitter", (c) => c.redirect(twitter));
 app.get("/youtube", (c) => c.redirect(youtube));
 app.get("/qiita", (c) => c.redirect(qiita));
+app.get("/getting_started", (c) => c.redirect(gettingStarted));
 
 serve(app.fetch);
