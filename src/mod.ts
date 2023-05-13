f519f62d4abd5aed85094d696462193f9697543c
lis2a <me@lilis.dev>
2023-05-13 13:00:00 +0900

    feat: zenn redirect endpoint

--- src/mod.ts
+++ src/mod.ts
@@ -8,6 +8,7 @@ const github = "https://github.com/codrawers";
 const twitter = "https://twitter.com/codrawers";
 const youtube = "https://youtube.com/@codrawers";
 const qiita = "https://qiita.com/organizations/codrawers";
+const zenn = "https://zenn.dev/p/codrawers"
 const gettingStarted =
   "https://codrawers.notion.site/80a3ccb317b2420693ca10ada089c0ca";
 
@@ -15,6 +16,7 @@ app.get("/github", (c) => c.redirect(github));
 app.get("/twitter", (c) => c.redirect(twitter));
 app.get("/youtube", (c) => c.redirect(youtube));
 app.get("/qiita", (c) => c.redirect(qiita));
+app.get("/zenn", (c) => c.redirect(zenn));
 app.get("/getting_started", (c) => c.redirect(gettingStarted));
 
 serve(app.fetch);
